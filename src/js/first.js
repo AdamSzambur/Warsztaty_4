df7465ba365bd1bc60de3732a25a203d7424b7f0
AdamSzambur <adamszamburski@gmail.com>
2019-06-26 22:45:29 +0200

    Warsztat 4 do oceny.

--- src/js/first.js
+++ src/js/first.js
@@ -2,8 +2,7 @@ $(function(){
 
 
     function ajax() {
-        var startOption =
-        {
+        var startOption = {
             url: "http://localhost:8282/books/",
             data: {},
             method: "GET",
@@ -16,7 +15,7 @@ $(function(){
             startOption.url = "http://localhost:8282"+$(arguments[0]).data('url');
             startOption.method = $(arguments[0]).data('type');
         }
-
+        // zamiana tabeli na obiekt
         if (arguments.length>1) {
             var result = {};
             for (var i=0; i<arguments[1].length; i++) {
@@ -56,27 +55,66 @@ $(function(){
                 '</div>' +
                 '</div>' +
                 '<div class="collapse" style="display:none">' +
-                '<div class="card-body"></div>' +
+                '<div class="card-body">' +
+                '<form class = "bookForm" data-url="/books/'+element.id+'" data-type="PUT">'+
+                '<div class="form-group row"><label for="bookId" class="col-sm-2 col-form-label">id</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookId" placeholder="bookId" name="id"></div></div>'+
+                '<div class="form-group row"><label for="bookIsbn" class="col-sm-2 col-form-label">isbn</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookIsbn" placeholder="bookIsbn" name="isbn"></div></div>'+
+                '<div class="form-group row"><label for="bookTitle" class="col-sm-2 col-form-label">title</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookTitle" placeholder="bookTitle" name="title"></div></div>'+
+                '<div class="form-group row"><label for="bookAuthor" class="col-sm-2 col-form-label">author</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookAuthor" placeholder="bookAuthor" name="author"></div></div>'+
+                '<div class="form-group row"><label for="bookPublisher" class="col-sm-2 col-form-label">publisher</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookPublisher" placeholder="bookPublisher" name="publisher"></div></div>'+
+                '<div class="form-group row"><label for="bookType" class="col-sm-2 col-form-label">type</label><div class="col-sm-3">'+
+                '<input type="text" class="form-control" id="bookType" placeholder="bookType" name="type"></div></div>'+
+                '<div class="form-group row"><div class="col-sm-10"><button type="submit" class="btn btn-success book-update">Zmień</button></div></div>'+
+                '</form>' +
+                '</div>' +
                 '</div>' +
                 '</div>');
+
+
+
+            var formBook = row.find('.bookForm');
+
+
+
+            formBook.on('submit',function(e){
+                e.preventDefault();
+                ajax(this,$(this).serializeArray());
+                refreshBooks();
+            });
+
+
             // dodwanie zadarzenia do elementu listy
             row.find('.book-title').on('click', function () {
                 var collapse = $(this).parent().parent().parent().find('.collapse');
                 var ajaxResult = ajax(this);
-                var description =
-                    'id : '+ ajaxResult.id + '<br>'+
-                    'isbn : '+ ajaxResult.isbn +'<br>'+
-                    'author : '+ ajaxResult.author +'<br>'+
-                    'publisher : '+ ajaxResult.publisher +'<br>'+
-                    'type : '+ ajaxResult.type;
+                // var description =
+                //     'id : '+ ajaxResult.id + '<br><br>'+
+                //     'isbn : '+ ajaxResult.isbn +'<br>'+
+                //     'author : '+ ajaxResult.author +'<br>'+
+                //     'publisher : '+ ajaxResult.publisher +'<br>'+
+                //     'type : '+ ajaxResult.type;
+
+                var bookForm =collapse.find('.bookForm');
+
+                console.log(bookForm);
+
 
                 if (collapse.css('display') === 'none') {
                     collapse.css('display', 'block');
-                    collapse.children(0).html(description);
-
+                     bookForm.find('[name=id]').attr('value',ajaxResult.id);
+                     bookForm.find('[name=isbn]').attr('value',ajaxResult.isbn);
+                     bookForm.find('[name=title]').attr('value',ajaxResult.title);
+                     bookForm.find('[name=author]').attr('value',ajaxResult.author);
+                     bookForm.find('[name=publisher]').attr('value',ajaxResult.publisher);
+                     bookForm.find('[name=type]').attr('value',ajaxResult.type);
                 } else {
                     collapse.css('display', 'none');
-                    collapse.children(0).html('');
+                    bookForm.find('[type = text]').attr('value','');
                 }
             });
 
